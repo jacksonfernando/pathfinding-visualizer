b231e3d624ed2855ea50c9916080312a0415bda9
jacksonfernando <jacksonfernando51@gmail.com>
2023-08-27 23:10:27 +0700

    add bfs algorithm

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -21,6 +21,41 @@ const Grid = () => {
 
   const [refArray, mm] = useState(getRefArray(grid))
 
+  function BFS(graph, hashmap, prevmap, start, target) {
+    let queue = [start]
+    let count = 0
+    hashmap[`${start.x}-${start.y}`] = true
+    while (queue.length > 0) {
+      count += 1
+      let c = queue.pop()
+      refArray[c.x + c.y * 50].current.style['transition-delay'] = `${count * 8}ms`
+      refArray[c.x + c.y * 50].current.classList.add('visited')
+      if (c.x == target.x && c.y == target.y) return [c, count]
+
+      if (c.x + 1 < 50 && !hashmap[`${c.x + 1}-${c.y}`] && !graph[c.y][c.x + 1].iswall) {
+        queue.unshift({ x: c.x + 1, y: c.y })
+        prevmap[`${c.x + 1}-${c.y}`] = { ...c }
+        hashmap[`${c.x + 1}-${c.y}`] = true
+      }
+      if (c.x - 1 >= 0 && !hashmap[`${c.x - 1}-${c.y}`] && !graph[c.y][c.x - 1].iswall) {
+        queue.unshift({ x: c.x - 1, y: c.y })
+        prevmap[`${c.x - 1}-${c.y}`] = { ...c }
+        hashmap[`${c.x - 1}-${c.y}`] = true
+      }
+      if (c.y + 1 < 25 && !hashmap[`${c.x}-${c.y + 1}`] && !graph[c.y + 1][c.x].iswall) {
+        queue.unshift({ x: c.x, y: c.y + 1 })
+        prevmap[`${c.x}-${c.y + 1}`] = { ...c }
+        hashmap[`${c.x}-${c.y + 1}`] = true
+      }
+      if (c.y - 1 >= 0 && !hashmap[`${c.x}-${c.y - 1}`] && !graph[c.y - 1][c.x].iswall) {
+        queue.unshift({ x: c.x, y: c.y - 1 })
+        prevmap[`${c.x}-${c.y - 1}`] = { ...c }
+        hashmap[`${c.x}-${c.y - 1}`] = true
+      }
+    }
+    return null
+  }
+
   function getRefArray(grid) {
     let array = []
     grid.forEach(elem => {

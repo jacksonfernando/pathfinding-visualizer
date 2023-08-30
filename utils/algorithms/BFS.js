19a06a3ecf6a502a436641563b6063de85c4f517
jacksonfernando <jacksonfernando51@gmail.com>
2023-08-30 23:37:22 +0700

    refactor BFS coordinate

--- utils/algorithms/BFS.js
+++ utils/algorithms/BFS.js
@@ -4,28 +4,34 @@ const BFS = (refArray, graph, hashmap, prevmap, start, target) => {
   hashmap[`${start.x}-${start.y}`] = true
   while (queue.length > 0) {
     count += 1
-    let c = queue.pop()
-    refArray[c.x + c.y * 5].current = true
-    if (c.x == target.x && c.y == target.y) return [c, count]
-    if (c.x + 1 < 5 && !hashmap[`${c.x + 1}-${c.y}`] && !graph[c.y][c.x + 1].iswall) {
-      queue.unshift({ x: c.x + 1, y: c.y })
-      prevmap[`${c.x + 1}-${c.y}`] = { ...c }
-      hashmap[`${c.x + 1}-${c.y}`] = true
+    let currentCoordinate = queue.pop()
+    let xCoordinate = currentCoordinate.x;
+    let yCoordinate = currentCoordinate.y;
+    refArray[xCoordinate + yCoordinate * 5].current = true
+    if (xCoordinate == target.x && yCoordinate == target.y) return [currentCoordinate, count]
+
+    if (xCoordinate + 1 < 5 && !hashmap[`${xCoordinate + 1}-${yCoordinate}`] && !graph[yCoordinate][xCoordinate + 1].iswall) {
+      queue.unshift({ x: xCoordinate + 1, y: yCoordinate })
+      prevmap[`${xCoordinate + 1}-${yCoordinate}`] = { ...currentCoordinate }
+      hashmap[`${xCoordinate + 1}-${yCoordinate}`] = true
     }
-    if (c.x - 1 >= 0 && !hashmap[`${c.x - 1}-${c.y}`] && !graph[c.y][c.x - 1].iswall) {
-      queue.unshift({ x: c.x - 1, y: c.y })
-      prevmap[`${c.x - 1}-${c.y}`] = { ...c }
-      hashmap[`${c.x - 1}-${c.y}`] = true
+
+    if (xCoordinate - 1 >= 0 && !hashmap[`${xCoordinate - 1}-${yCoordinate}`] && !graph[yCoordinate][xCoordinate - 1].iswall) {
+      queue.unshift({ x: xCoordinate - 1, y: yCoordinate })
+      prevmap[`${xCoordinate - 1}-${yCoordinate}`] = { ...currentCoordinate }
+      hashmap[`${xCoordinate - 1}-${yCoordinate}`] = true
     }
-    if (c.y + 1 < 5 && !hashmap[`${c.x}-${c.y + 1}`] && !graph[c.y + 1][c.x].iswall) {
-      queue.unshift({ x: c.x, y: c.y + 1 })
-      prevmap[`${c.x}-${c.y + 1}`] = { ...c }
-      hashmap[`${c.x}-${c.y + 1}`] = true
+
+    if (yCoordinate + 1 < 5 && !hashmap[`${xCoordinate}-${yCoordinate + 1}`] && !graph[yCoordinate + 1][xCoordinate].iswall) {
+      queue.unshift({ x: xCoordinate, y: yCoordinate + 1 })
+      prevmap[`${xCoordinate}-${yCoordinate + 1}`] = { ...currentCoordinate }
+      hashmap[`${xCoordinate}-${yCoordinate + 1}`] = true
     }
-    if (c.y - 1 >= 0 && !hashmap[`${c.x}-${c.y - 1}`] && !graph[c.y - 1][c.x].iswall) {
-      queue.unshift({ x: c.x, y: c.y - 1 })
-      prevmap[`${c.x}-${c.y - 1}`] = { ...c }
-      hashmap[`${c.x}-${c.y - 1}`] = true
+
+    if (yCoordinate - 1 >= 0 && !hashmap[`${xCoordinate}-${yCoordinate - 1}`] && !graph[yCoordinate - 1][xCoordinate].iswall) {
+      queue.unshift({ x: xCoordinate, y: yCoordinate - 1 })
+      prevmap[`${xCoordinate}-${yCoordinate - 1}`] = { ...currentCoordinate }
+      hashmap[`${xCoordinate}-${yCoordinate - 1}`] = true
     }
   }
   return null

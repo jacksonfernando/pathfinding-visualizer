cf6ef4f915340f3ab3384078a216c193fc9a851f
jacksonfernando <jacksonfernando51@gmail.com>
2023-09-02 22:16:12 +0700

    add refArray in DFS algorithm

--- utils/algorithms/DFS.js
+++ utils/algorithms/DFS.js
@@ -1,10 +1,11 @@
-const iterateRecursively = (graph, x, y, hashMap, prevMap, target) => {
+const iterateRecursively = (refArray, graph, x, y, hashMap, prevMap, target) => {
   if (graph[y][x].x == target.x && graph[y][x].y == target.y) {
     return [{ x: x, y: y }]
   }
   if (x < 0 || x >= 5 || y < 0 || y >= 5) {
     return null;
   }
+  refArray[x + y * 5].current = true;
   return iterateRecursively(graph, x + 1, y, hashMap, prevMap, target) ||
     iterateRecursively(graph, x - 1, y, hashMap, prevMap, target) ||
     iterateRecursively(graph, x, y + 1, hashMap, prevMap, target) ||
@@ -12,7 +13,7 @@ const iterateRecursively = (graph, x, y, hashMap, prevMap, target) => {
 }
 
 const DFS = (refArray, graph, hashMap, prevMap, start, target) => {
-  return iterateRecursively(graph, start.x, start.y, hashMap, prevMap, target);
+  return iterateRecursively(refArray, graph, start.x, start.y, hashMap, prevMap, target);
 }
 
 export {

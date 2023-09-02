16a052f2c556c582a5fbd679ebcee3c3853061bd
jacksonfernando <jacksonfernando51@gmail.com>
2023-09-02 22:07:41 +0700

    add recursive iterating with dfs

--- utils/algorithms/DFS.js
+++ utils/algorithms/DFS.js
@@ -1,4 +1,13 @@
+const iterateRecursively = (graph, x, y, hashMap, prevMap) => {
+  iterateRecursively(graph, x + 1, y, hashMap, prevMap);
+  iterateRecursively(graph, x - 1, y, hashMap, prevMap);
+  iterateRecursively(graph, x, y + 1, hashMap, prevMap);
+  iterateRecursively(graph, x, y - 1, hashMap, prevMap);
+}
+
 const DFS = (refArray, graph, hashMap, prevMap, start, target) => {
+
+  DFS(refArray, graph, hashMap, prevMap, start, target);
 }
 
 export {

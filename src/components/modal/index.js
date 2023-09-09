6c657f3b49f0fe3ccdc4c77749e33d234b3e10c6
jacksonfernando <jacksonfernando51@gmail.com>
2023-09-09 22:19:22 +0700

    move close icon to right

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -4,13 +4,17 @@ import { IoCloseOutline } from "react-icons/io5";
 const Modal = ({ content, open, setIsModalOpen }) => {
   return open && (
     <div className={modalStyle.container}>
-      <IoCloseOutline
-        key='closeMenu'
-        size={33}
-        onClick={() => setIsModalOpen(false)}
-      />
-      {content}
-    </div>
+      <div className={modalStyle.closeIcon}>
+        <IoCloseOutline
+          key='closeMenu'
+          size={33}
+          onClick={() => setIsModalOpen(false)}
+        />
+      </div>
+      <div className={modalStyle.content}>
+        {content}
+      </div >
+    </div >
   )
 }
 

f2e579579144a3917f6fbeafd7c6a4d4b24d92ad
jacksonfernando <jacksonfernando51@gmail.com>
2023-09-10 22:56:53 +0700

    set algo when clicking bfs or dfs

--- src/components/modal/AlgorithmsModal.js
+++ src/components/modal/AlgorithmsModal.js
@@ -1,11 +1,15 @@
 import Modal from "."
+import { BFS_ALGORITHM, DFS_ALGORITHM } from "../../../constants/global"
+import { useParams } from "../../../store/context"
 
 const Algorithmsmodal = ({ open, setIsModalOpen }) => {
+  const { setAlgo } = useParams()
+
   const AlgorithmContent = () => {
     return (
       <div>
-        <p>BFS</p>
-        <p>DFS</p>
+        <p onClick={setAlgo(BFS_ALGORITHM)}>BFS</p>
+        <p onClick={setAlgo(DFS_ALGORITHM)}>DFS</p>
         <p>Djakstra</p>
       </div>
     )

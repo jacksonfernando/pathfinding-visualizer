4ccb33c6e6bcfaa3937ac348dd2285b47822a7f8
jacksonfernando <jacksonfernando51@gmail.com>
2023-09-12 21:33:27 +0700

    add djakstra algorithm

--- src/components/modal/AlgorithmsModal.js
+++ src/components/modal/AlgorithmsModal.js
@@ -1,25 +1,24 @@
 import Modal from "."
-import { BFS_ALGORITHM, DFS_ALGORITHM } from "../../../constants/global"
+import { BFS_ALGORITHM, DFS_ALGORITHM, DJAKSTRA_ALGORITHM } from "../../../constants/global"
 import { useParams } from "../../../store/context"
 
 const Algorithmsmodal = ({ open, setIsModalOpen }) => {
   const { setAlgo } = useParams()
+
+  const setAlgoAndCloseModal = (algo) => {
+    setAlgo(algo)
+    setIsModalOpen(false)
+  }
 
   const AlgorithmContent = () => {
     return (
       <div>
         <h2>Choose your Algorithm!</h2>
         <br />
-        <p onClick={() => {
-          setAlgo(BFS_ALGORITHM)
-          setIsModalOpen(false)
-        }}>BFS</p>
-        <p onClick={() => {
-          setAlgo(DFS_ALGORITHM)
-          setIsModalOpen(false)
-        }}>DFS</p>
-        <p>Djakstra</p>
-      </div>
+        <p onClick={setAlgoAndCloseModal(BFS_ALGORITHM)}>BFS</p>
+        <p onClick={setAlgoAndCloseModal(DFS_ALGORITHM)}>DFS</p>
+        <p onClick={setAlgoAndCloseModal(DJAKSTRA_ALGORITHM)}>Djakstra</p>
+      </div >
     )
   }
   return open && (

74421bf0168b3fe49e5f75ae2272453e185cf8ae
jacksonfernando <jacksonfernando51@gmail.com>
2023-09-19 22:34:47 +0700

    delete neighbours visited when checking neigbours

--- utils/algorithms/Dijkstra.js
+++ utils/algorithms/Dijkstra.js
@@ -2,19 +2,15 @@ const checkNeighboursAndPushToQueue = (queue, graph, currentCell, hashMap, width
   const { x, y } = currentCell;
   if (x + 1 < height && !hashMap[`${x + 1}-${y}`]) {
     currentCell.neighbours.push(graph[x + 1][y])
-    hashMap[`${x + 1}-${y}`] = true
   }
   if (x - 1 >= 0 && !hashMap[`${x - 1}-${y}`]) {
     currentCell.neighbours.push(graph[x - 1][y])
-    hashMap[`${x - 1}-${y}`] = true
   }
   if (y + 1 < width && !hashMap[`${x}-${y + 1}`]) {
     currentCell.neighbours.push(graph[x][y + 1])
-    hashMap[`${x}-${y + 1}`] = true
   }
   if (y - 1 >= 0 && !hashMap[`${x}-${y - 1}`]) {
     currentCell.neighbours.push(graph[x][y - 1])
-    hashMap[`${x}-${y - 1}`] = true
   }
   for (let cell of currentCell.neighbours) {
     queue.push(cell)

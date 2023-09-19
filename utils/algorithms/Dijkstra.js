5f32c6b225e755812e9931923b9e7ad003aa72c1
jacksonfernando <jacksonfernando51@gmail.com>
2023-09-19 23:11:37 +0700

    add back visited in every neighbours

--- utils/algorithms/Dijkstra.js
+++ utils/algorithms/Dijkstra.js
@@ -2,15 +2,19 @@ const checkNeighboursAndPushToQueue = (queue, graph, currentCell, hashMap, width
   const { x, y } = currentCell;
   if (x + 1 < height && !hashMap[`${x + 1}-${y}`]) {
     currentCell.neighbours.push(graph[x + 1][y])
+    hashMap[`${x + 1}-${y}`] = true
   }
   if (x - 1 >= 0 && !hashMap[`${x - 1}-${y}`]) {
     currentCell.neighbours.push(graph[x - 1][y])
+    hashMap[`${x - 1}-${y}`] = true
   }
   if (y + 1 < width && !hashMap[`${x}-${y + 1}`]) {
     currentCell.neighbours.push(graph[x][y + 1])
+    hashMap[`${x}-${y + 1}`] = true
   }
   if (y - 1 >= 0 && !hashMap[`${x}-${y - 1}`]) {
     currentCell.neighbours.push(graph[x][y - 1])
+    hashMap[`${x}-${y - 1}`] = true
   }
   for (let cell of currentCell.neighbours) {
     queue.push(cell)
@@ -22,13 +26,13 @@ const dijkstra = (refArray, graph, hashMap, start, target) => {
   const MAX_HEIGHT = graph.length;
   const MAX_WIDTH = graph[0].length;
   while (queue.length > 0) {
-    const currentCell = queue.pop();
+    const currentCell = queue.shift();
     refArray[currentCell.y + (currentCell.x * MAX_WIDTH)].current = true;
     if (currentCell.x == target.x && currentCell.y == target.y) {
       return [currentCell]
     }
     if (hashMap[`${currentCell.x}-${currentCell.y}`]) {
-      queue.pop();
+      queue.shift();
     }
     checkNeighboursAndPushToQueue(queue, graph, currentCell, hashMap, MAX_WIDTH, MAX_HEIGHT)
     hashMap[`${currentCell.x}-${currentCell.y}`] = true;

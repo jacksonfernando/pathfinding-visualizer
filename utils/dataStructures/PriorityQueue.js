3a621f8cce09fb80166e929212228142d2a49a42
jacksonfernando <jacksonfernando51@gmail.com>
2023-09-21 22:14:07 +0700

    implement sift up priority queue

--- utils/dataStructures/PriorityQueue.js
+++ utils/dataStructures/PriorityQueue.js
@@ -1,3 +1,5 @@
+const { swapItemsInArray } = require("../global");
+
 class PriorityQueue {
   #elements;
   #compare;
@@ -15,4 +17,34 @@ class PriorityQueue {
   peek() {
     return this.#elements[0];
   }
+
+  size() {
+    return this.#elements.length;
+  }
+
+  static parent(index) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  static left(index) {
+    return index * 2 + 1;
+  }
+
+  static right(index) {
+    return index * 2 + 2;
+  }
+
+  #siftUp(index) {
+    let parentIndex = PriorityQueue.parent(index);
+
+    while (
+      parentIndex >= 0 &&
+      this.#compare(this.#elements[index], this.#elements[parentIndex])) {
+      swapItemsInArray(this.#elements, parentIndex, index);
+      index = parentIndex
+      parentIndex = PriorityQueue.parent(index)
+    }
+  }
 }
+
+export default PriorityQueue

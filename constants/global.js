9ae733a855bbe6bfe2eec5fd4a4e0b51d3f7de35
jacksonfernando <jacksonfernando51@gmail.com>
2023-09-27 22:29:13 +0700

    width 10 and height of 30 to cater to mobile responsiveness

--- constants/global.js
+++ constants/global.js
@@ -1,8 +1,8 @@
 const NAVBAR_TITLE = 'Pathfinding Visualizer';
 
-const DEFAULT_WIDTH = 40;
+const DEFAULT_WIDTH = 10;
 
-const DEFAULT_HEIGHT = 20;
+const DEFAULT_HEIGHT = 40;
 
 const DFS_ALGORITHM = 'DFS';
 
